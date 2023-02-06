eb04f3455552b71601d7bc4b0190b2226dff778a
iO Academy <hamyas@gmail.com>
2023-02-06 17:00:58 +0000

    created function to get data from database

--- src/db.php
+++ src/db.php
@@ -1,7 +1,13 @@
 <?php
 
 function connectToDb () {
-    $db = new PDO ('mysql:host=db;dbname=chickensOfTheWoods', 'root', 'password');
+    $db = new PDO ('mysql:host=db;dbname=deitybase', 'root', 'password');
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $db;
+}
+
+function getAllDeities (PDO $db) {
+    $query = $db->prepare('SELECT * FROM deities');
+    $query->execute();
+    return $query->fetchAll();
 }
