b5d89469eb5732e33721f7eaf3b077f5b16d3444
iO Academy <hamyas@gmail.com>
2023-02-06 16:06:00 +0000

    corrected html, added mock text

--- index.php
+++ index.php
@@ -2,19 +2,20 @@
 
 ?>
 
-<DOCTYPE! html>
+<!DOCTYPE html>
 
 <html>
-<header>
+<head>
     <title>Deitybase</title>
-</header>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+</head>
 <body>
     <h1>BEHOLD!! Some deities and stuff:</h1>
     <div>
-        <h2>Name</h2>
-        <p>From</p>
-        <p>Assoc</p>
-        <p>Object</p>
+        <h2>Annoia</h2>
+        <p>From the Discworld</p>
+        <p>Associated to objects getting stuck in the cutlery drawer</p>
+        <p>Carries no specific object</p>
     </div>
 </body>
 </html>
