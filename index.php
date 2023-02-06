8977f9df1601c3a0e98c72442e4ea69b51855b90
iO Academy <hamyas@gmail.com>
2023-02-06 15:37:49 +0000

    created basic file structure and rough html layout

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<DOCTYPE! html>
+
+<html>
+<header>
+    <title>Deitybase</title>
+</header>
+<body>
+    <h1>BEHOLD!! Some deities and stuff:</h1>
+    <div>
+        <h2>Name</h2>
+        <p>From</p>
+        <p>Assoc</p>
+        <p>Object</p>
+    </div>
+</body>
+</html>
